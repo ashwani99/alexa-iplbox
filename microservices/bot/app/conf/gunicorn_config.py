76309a0261f537496a7930710d539a5dfe2ca052
Ashwani Gupta <ashwanigupta125@gmail.com>
2018-02-05 21:02:44 +0530

    Change number of threads to 2 to handle multiple requests

--- microservices/bot/app/conf/gunicorn_config.py
+++ microservices/bot/app/conf/gunicorn_config.py
@@ -3,6 +3,7 @@ import multiprocessing
 
 bind = "0.0.0.0:8080"
 workers = 1
+threads = 2
 accesslog = "-"
 access_log_format = '%(h)s %(l)s %(u)s %(t)s "%(r)s" %(s)s %(b)s "%(f)s" "%(a)s"'
 loglevel = "debug"
