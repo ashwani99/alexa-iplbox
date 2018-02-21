2e7ae0ce64473db10c8168baecf4eaf5a69132c1
Ashwani Gupta <ashwanigupta125@gmail.com>
2018-02-22 03:02:37 +0530

    Refactor code

--- microservices/bot/app/src/__init__.py
+++ microservices/bot/app/src/__init__.py
@@ -1,5 +1,7 @@
 from flask import Flask
+from flask_ask import Ask
 
 app = Flask(__name__)
+ask = Ask(app, '/alexa-iplbox')
 
 from src import alexa_iplbox
--- microservices/bot/app/src/alexa_iplbox.py
+++ microservices/bot/app/src/alexa_iplbox.py
@@ -1,67 +1,44 @@
 import os
 import json
-from flask_ask import Ask, statement, question
+from flask_ask import statement, question, session
 from flask_ask import request as flask_ask_request
 from flask import render_template, make_response, request, url_for, jsonify
 import requests
-from src import app
+from src import app, ask
 
 PRODUCTION_ENV = os.environ.get('PRODUCTION')
 CLUSTER_NAME = os.environ.get('CLUSTER_NAME')
+ACCESS_TOKEN = os.environ.get('DATA_ADMIN_TOKEN')
 
 if CLUSTER_NAME is None:
     print('CLUSTER_NAME not found. Please export environment variable CLUSTER_NAME=<name_of_your_hasura_cluster>')
 
-
-ask = Ask(app, '/alexa-iplbox')
-
-# Store latest log data as dict. TODO: Update this
-query_log = {
-    'timestamp': None,
-    'query_text': None,
-    'answer': None,
-    'response_time': None,
-    'is_error_occured': None
-}
+if PRODUCTION_ENV:
+    DATA_URL = "http://data.hasura/v1/query"
+else:
+    DATA_URL = "https://data." + CLUSTER_NAME + ".hasura-app.io/v1/query"
 
 
 @app.route('/')
 @app.route('/index')
 def home():
+    '''Dummy welcome text when API endpoint is accessed'''
     return 'Alexa IPLBox is running now.'
 
 
 @ask.launch
 def handle_launch():
+    '''Handles LaunchRequest when the user invokes the skill with the invocation name'''
     welcome_text = 'Welcome to IPL Box... Ask me anything about the Indian Premier League...'
     return question(welcome_text)
 
 
 @ask.intent('GetWinnerIntent')
 def get_winner(season):
-    # SEASON_WORD_TO_YEAR = {
-    #     '1st': 2008,
-    #     '2nd': 2009,
-    #     '3rd': 2010,
-    #     '4th': 2011,
-    #     '5th': 2012,
-    #     '6th': 2013,
-    #     '7th': 2014,
-    #     '8th': 2015,
-    #     '9th': 2016,
-    #     '10th': 2017
-    # }
-    # if season_word:
-    #     season = SEASON_WORD_TO_YEAR[season_word]
-    # else:
-    #     season = season_numeric
-    if PRODUCTION_ENV:
-        data_url = "http://data.hasura/v1/query"
-    else:
-        data_url = "https://data." + CLUSTER_NAME + ".hasura-app.io/v1/query"
+    '''Responds with the name of the winning team in a given year'''
     headers = {
         "Content-Type": "application/json",
-        "Authorization": os.environ.get('DATA_ADMIN_TOKEN')
+        "Authorization": ACCESS_TOKEN
     }
     body = {
         "type": "run_sql",
@@ -72,39 +49,45 @@ def get_winner(season):
                     ORDER BY season, date DESC;".format(season)
         }
     }
-    response = requests.post(data_url, data=json.dumps(body), headers=headers)
+    response = requests.post(DATA_URL, data=json.dumps(body), headers=headers)
     json_response = response.json()
-    # print(json_response) # for testing purpose
-    is_error_occured = False
+    session.attributes['is_error_occured'] = False # Setting default value to no errors occured
     if 'result' in json_response:
         if len(json_response['result']) > 1: # check if result is actually available
             winner = json_response['result'][1][0]
-            answer = 'IPL Box tells me that the {} season of Indian Premier League was won by {}'.format(season, winner)
+            session.attributes['query_response'] = 'IPL Box tells me that the {} season of Indian Premier League was won by {}'.format(season, winner)
         else:
-            answer = 'Sorry, I couldn\'t find any IPL season in year... {}'.format(season)
-            is_error_occured = True
+            session.attributes['query_response'] = 'Sorry, I couldn\'t find any IPL season in year... {}'.format(season)
+            session.attributes['is_error_occured'] = True
     else:
-        answer = 'Sorry, I couldn\'t find any IPL season in year... {}'.format(season)
-        is_error_occured = True
-    log_payload = {
-        'timestamp': str(flask_ask_request.timestamp),
-        'query_text': 'Which team won the IPL in {}'.format(season), # Hardcoded value, TODO: update this
-        'answer': answer,
-        'response_time': response.elapsed.total_seconds(),
-        'is_error_occured': is_error_occured
+        session.attributes['query_response'] = 'Sorry, I couldn\'t find any IPL season in year... {}'.format(season)
+        session.attributes['is_error_occured'] = True
+    session.attributes['query_text'] = 'Which team won the IPL in {}'.format(season), # Hardcoded value, TODO: update this
+    session.attributes['response_time'] = response.elapsed.total_seconds()
+    update_log()
+    return statement(session.attributes['query_response'])
+
+
+def update_log():
+    '''Logs the latest query/response to the database'''
+    body = {
+        'type': 'insert',
+        'args': {
+            'table': 'logs',
+            'objects': [
+                {
+                    'user_id': session.user.userId,
+                    'timestamp': flask_ask_request.timestamp.isoformat(),
+                    'query_text': session.attributes['query_text'],
+                    'query_response': session.attributes['query_response'],
+                    'response_time': session.attributes['response_time'],
+                    'is_error_occured': session.attributes['is_error_occured']
+                }
+            ]
+        }
     }
-    headers = {'Content-Type': 'application/json'}
-    logs_url = 'https://bot.{}.hasura-app.io/logs'.format(CLUSTER_NAME)
-    requests.put(logs_url, data=json.dumps(log_payload), headers=headers) # make a PUT request to /logs to update new log value
-    return statement(answer)
-
-
-@app.route('/logs', methods=['GET', 'PUT'])
-def send_log():
-    '''Send logs to /logs endpoint'''
-    if request.method == 'GET':
-        return jsonify(query_log)
-    elif request.method == 'PUT':
-        for key, value in request.get_json().items():
-            query_log[key] = value
-        return 'Successfully updated log', 202
+    headers = {
+        "Content-Type": "application/json",
+        "Authorization": ACCESS_TOKEN
+    }
+    requests.post(DATA_URL, data=json.dumps(body), headers=headers)
